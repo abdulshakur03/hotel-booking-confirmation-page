6285d6ba4e84778bc621eaf3d7a822b62899a69f
abdulshakur <abdulshakurmahmood@gmail.com>
2026-09-17 17:14:08 +0100

    continue from bigcard

---
+++ src/components/BigWhiteCard.jsx
@@ -0,0 +1,21 @@
+export default function BigWhiteCard() {
+  return (
+    <div className="big-card-container">
+      <hr class="dashed-line" />
+
+      <span className="content">
+        <p className="welcome">WELCOME CARD</p>
+        <img src="src/assets/images/icon-sun.svg" alt="sun" className="sunny" />
+        <p className="host-note-heading">A note from your host,</p>
+        <h1 className="host-name">Margaux.</h1>
+        <p className="host-note">
+          we're so glad you're coming. The shutters will be open, the lemonade
+          cold, and the cat -Poivre-Pretending not to notice you.
+        </p>
+
+        <p className="room">ROOM</p>
+        <p className="room-name">La Garrigue</p>
+      </span>
+    </div>
+  );
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,19 @@
 import "../css/Main.css";
+import BigCard from "./BigOrangeCard";
 export default function Main() {
   return (
     <div className="main">
       <div className="booking-info">
         BOOKING<span>.</span>CONFIRMED
       </div>
+      <h1 className="title">
+        Bienvenue,<span>Lucia.</span>
+      </h1>
+      <div className="option-btn">
+        <button>Print receipt</button>
+        <button>Add to calendar</button>
+      </div>
+      <BigCard />
     </div>
   );
 }
